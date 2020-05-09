63080b807d489a556e49357234cb57bede171fd6
Jonathan Israel Vásquez Vivas <jon2013.p@hotmail.com>
2020-05-09 12:44:47 -0500

    Controlador de Article

--- routes/api.php
+++ routes/api.php
@@ -19,26 +19,12 @@ use Illuminate\Support\Facades\Route;
 //    return $request->user();
 //});
 
-Route::get('articles', function (){
-    return Article::all();
-});
+Route::get('articles', 'ArticleController@index');
 
-Route::get('articles/{id}', function ($id){
-    return Article::find($id);
-});
+Route::get('articles/{article}', 'ArticleController@show');
 
-Route::post('articles', function (Request $request){
-    return Article::create($request->all);
-});
+Route::post('articles','ArticleController@store');
 
-Route::post('articles', function (Request $request){
-    return Article::create($request->all());
-});
+Route::post('articles','ArticleController@update');
 
-Route::put('articles/{id}', function (Request $request,$id){ $article = Article::findOrFail($id); $article->update($request->all());
-    return $article;
-});
-
-Route::delete('articles/{id}', function ($id) { Article::find($id)->delete();
-    return 204;
-});
+Route::delete('articles/{article}','ArticleController@delete');
